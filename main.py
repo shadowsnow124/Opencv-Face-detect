dfa8ae155e868c6b9d7609f0dfc96230d36a07da
ShadowSnow <99937524+shadowsnow124@users.noreply.github.com>
2025-08-09 13:05:32 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import cv2
 
-# Load the pre-trained Haarcascades face detector
+# Load the model
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
 # Open a video capture stream
@@ -10,17 +10,16 @@ while True:
     # Read a frame 
     ret, frame = cap.read()
 
-    # Convert the frame to grayscale 
+    # Convert to grayscale 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     # Detect faces in the frame
     faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5, minSize=(30, 30))
 
-    # Draw rectangles 
+    # Draw rectangle 
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
-
-    # Display 
+ 
     cv2.imshow('Face Detection', frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -28,3 +27,4 @@ while True:
 
 cap.release()
 cv2.destroyAllWindows()
+
